386d7b8b83fdab5fb39bba08c68401e88b6965ef
ilia morozov <ilia.m.composer@gmail.com>
2024-01-10 10:37:30 +0000

    testing notifications

--- src/components/PushTest/PushTest.tsx
+++ src/components/PushTest/PushTest.tsx
@@ -1,9 +1,46 @@
 import { useEffect } from "react";
 
 function PushTest() {
-  
+  useEffect(() => {
+    const registerServiceWorker = async () => {
+      try {
+        const sw = await navigator.serviceWorker.register('../notifications/sw.js');
+        console.log('Service Worker registered:', sw);
+      } catch (error) {
+        console.error('Error registering Service Worker:', error);
+      }
+    };
 
-  return <div><button> SUBSCRIBE</button></div>;
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', registerServiceWorker);
+    }
+
+    // Cleanup the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener('load', registerServiceWorker);
+    };
+  }, []);
+
+  const subscribeToPush = async () => {
+    try {
+      const registration = await navigator.serviceWorker.ready;
+      const subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: 'BMAm0ZdfJ_pa5ec_Q17CAnhPLlE2mXcrv13ZMAVY2EESrT2piQf4Y0FvD5nWU39_Dh1XxytD43J7BYY6DFsk0Jo', // Replace with your actual public key
+      });
+
+      // Send the subscription object to your server for future use
+      console.log('Subscription:', JSON.stringify(subscription));
+    } catch (error) {
+      console.error('Error subscribing to push notifications:', error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={subscribeToPush}>SUBSCRIBE</button>
+    </div>
+  );
 }
 
 export default PushTest;
---
+++ src/notifications/push.js
@@ -0,0 +1,31 @@
+const webpush = require('web-push');
+
+// VAPID keys should be generated only once.
+let vapidKeys = {
+  publicKey: 'BMAm0ZdfJ_pa5ec_Q17CAnhPLlE2mXcrv13ZMAVY2EESrT2piQf4Y0FvD5nWU39_Dh1XxytD43J7BYY6DFsk0Jo',
+  privateKey: 'WFcvWtVGhofEZwYiZTRVLnfVs8fvcHLArdHT66zZ1-g'
+}
+
+webpush.setVapidDetails('mailto:test@code.co.uk',vapidKeys.publicKey,vapidKeys.privateKey);
+
+let sub = {}
+webpush.sendNotification(sub,'test message');
+
+// webpush.setGCMAPIKey('<Your GCM API Key Here>');
+// webpush.setVapidDetails(
+//   'mailto:example@yourdomain.org',
+//   vapidKeys.publicKey,
+//   vapidKeys.privateKey
+// );
+
+// console.log(vapidKeys)
+// This is the same output of calling JSON.stringify on a PushSubscription
+// const pushSubscription = {
+//   endpoint: '.....',
+//   keys: {
+//     auth: '.....',
+//     p256dh: '.....'
+//   }
+// };
+
+// webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
