839d7fcf7a21ed4fcd5d03185a7e8352038591ab
ilia morozov <ilia.m.composer@gmail.com>
2024-01-17 14:31:35 +0000

    chabged routes settings

--- src/components/Server/CenterPanel/CenterPanel.tsx
+++ src/components/Server/CenterPanel/CenterPanel.tsx
@@ -3,14 +3,15 @@ import Servers from './Servers/Servers';
 import s from "./CenterPanel.module.scss"
 import Flow from './Flow/Flow';
 import Security from './Security/Security';
+import ProtectedRoute from '../../../utils/ProtectedRoute';
 
 function CenterPanel() {
     return (
         <div className={s.wrapper}>
             <Routes>
-                <Route path='/servers' element={<Servers></Servers>}></Route>
-                <Route path='/flows' element={<Flow></Flow>}></Route>
-                <Route path='/security' element={<Security></Security>}></Route>
+                <Route  path='/servers' element={<ProtectedRoute><Servers></Servers></ProtectedRoute> }></Route>
+                <Route path='/flows' element={<ProtectedRoute><Flow></Flow></ProtectedRoute> }></Route>
+                <Route path='/security' element={<ProtectedRoute><Security></Security></ProtectedRoute>}></Route>
             </Routes>
         </div>)
 }
