8d53f80992a3e12406485ca4191db6aaa87c018b
ilia morozov <ilia.m.composer@gmail.com>
2024-02-06 12:31:26 +0000

    finished settings component

--- src/components/Server/CenterPanel/Settings/Settings.tsx
+++ src/components/Server/CenterPanel/Settings/Settings.tsx
@@ -12,25 +12,45 @@ interface ISetting {
 
 function Settings() {
   const [settingsList, setSettingsList] = useState<Array<ISetting>>();
+  const [defaultSettings, setDefaultSettings] = useState<Array<ISetting>>();
 
   const fetchSettings = async () => {
     try {
       const res: any = await getSettingsAPI();
+      console.log(res)
       const sort = res.data.sort((a: ISetting) => (a.readOnly ? 1 : -1));
 
+      setDefaultSettings(sort);
       setSettingsList(sort);
     } catch (e) {
       console.log(e);
     }
   };
 
-  const updateSetting = async (settingId: number, value: string) => {
-    try {
-      const res: any = await updateSettingAPI(settingId, value);
-      console.log(res);
-    } catch (e) {
-      console.log(e);
+  const updateSetting = async () => {
+
+    const changedSettings: Array<ISetting> = [];
+    if (settingsList && defaultSettings) {
+      for (let i = 0; i < settingsList.length - 1; i++) {
+        if (settingsList[i].value !== defaultSettings[i].value) {
+          changedSettings.push(settingsList[i]);
+        }
+      }
     }
+
+    if (changedSettings.length > 0) {
+      changedSettings.forEach(async (setting: ISetting) => {
+        try {
+          const res: any = await updateSettingAPI(setting.id, setting.value);
+          console.log(res);
+        }
+
+        catch (e) {
+          console.log(e);
+        }
+      })
+    }
+
   };
 
   function setInputType(setting: ISetting) {
@@ -71,40 +91,34 @@ function Settings() {
       <ul>
         {settingsList && settingsList?.length > 0
           ? settingsList.map((setting: ISetting) => (
-              <li key={setting.id}>
-                <div className={s.setting_wrapper}>
-                  <div className={s.title_and_description}>
-                    <header>{setting.name}</header>
-                    <span>({setting.description})</span>
-                  </div>
-                  <div className={s.input_and_submit}>
-                    <input
-                      type={setInputType(setting)}
-                      value={setting.value}
-                      checked={
-                        setting.value.toLocaleLowerCase() === "true"
-                          ? true
-                          : false
-                      }
-                      readOnly={setting.readOnly}
-                      disabled={setting.readOnly}
-                      onChange={(e: any) =>
-                        handleValueChange(setting, e.target.value)
-                      }
-                    />
-                    {setting.readOnly === false ? (
-                      <button
-                        onClick={() => updateSetting(setting.id, setting.value)}
-                      >
-                        Save
-                      </button>
-                    ) : null}
-                  </div>
+            <li key={setting.id}>
+              <div className={s.setting_wrapper}>
+                <div className={s.title_and_description}>
+                  <header>{setting.name}</header>
+                  <span>({setting.description})</span>
                 </div>
-              </li>
-            ))
-          : null}
+                <div className={s.input_and_submit}>
+                  <input
+                    type={setInputType(setting)}
+                    value={setting.value}
+                    checked={
+                      setting.value.toLocaleLowerCase() === "true"
+                        ? true
+                        : false
+                    }
+                    readOnly={setting.readOnly}
+                    disabled={setting.readOnly}
+                    onChange={(e: any) =>
+                      handleValueChange(setting, e.target.value)
+                    }
+                  />
+                </div>
+              </div>
+            </li>
+          ))
+          : <li>Settings are not available</li>}
       </ul>
+      {settingsList && settingsList?.length > 0 ? <div className={s.input_and_submit}><button onClick={updateSetting}>SAVE</button></div> : null}
     </div>
   );
 }
