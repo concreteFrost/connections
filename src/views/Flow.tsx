893fa31345d4b60e3593de36406c51b5efc497db
concreteFrost <ilia.m.composer@gmail.com>
2024-07-01 10:10:20 +0100

    temp removed copy paste feature

--- src/views/Flow.tsx
+++ src/views/Flow.tsx
@@ -32,24 +32,9 @@ function Flow(props: any) {
     [flowSlice.flow.visual.blocks]
   );
 
-  const { reactFlowInstance, reactFlowWrapper, setInstance, setFlowWrapper } =
-    useStore((state) => state.designerVisualElementsSlice);
-  const { snapToGrid, snapStep } = useStore(
-    (state) => state.topPanelSlice.settings
-  );
 
-  const [isCtrlPressed, setCtrlPressed] = useState(false);
-  useCtrlMouseHold(isCtrlPressed, setCtrlPressed);
-
-  function pasteCopiedBlock(event: ReactMouseEvent) {
-    var pos = positionInViewport(event, reactFlowInstance, reactFlowWrapper);
-    if (isCtrlPressed) {
-      const selectedBlock = getSelectedBlock(flowSlice);
-      if (selectedBlock) {
-        flowSlice.createBlockCopy(Math.round(pos.x), Math.round(pos.y));
-      }
-    }
-  }
+  const { reactFlowInstance, reactFlowWrapper, setInstance, setFlowWrapper } =useStore((state) => state.designerVisualElementsSlice);
+  const { snapToGrid, snapStep } = useStore((state) => state.topPanelSlice.settings);
 
   return (
     <div
@@ -70,10 +55,11 @@ function Flow(props: any) {
         snapToGrid={snapToGrid}
         snapGrid={[snapStep[0], snapStep[1]]}
         deleteKeyCode={null}
-        onMouseUp={(e: any) => {
-          pasteCopiedBlock(e);
-        }}
-        nodesDraggable={!isCtrlPressed}
+
+        // onMouseUp={(e: any) => {
+        //   pasteCopiedBlock(e);
+        // }}
+        // nodesDraggable={!isCtrlPressed}
       >
         <Background
           color={`rgb(74, 148, 190, ${
@@ -87,3 +73,18 @@ function Flow(props: any) {
 }
 
 export default Flow;
+
+//COPY/PASTE BLOCK ON HOLD CTRL BTN
+//EXPERIMENTAL FEATURE 
+// const [isCtrlPressed, setCtrlPressed] = useState(false);
+// useCtrlMouseHold(isCtrlPressed, setCtrlPressed);
+
+// function pasteCopiedBlock(event: ReactMouseEvent) {
+//   var pos = positionInViewport(event, reactFlowInstance, reactFlowWrapper);
+//   if (isCtrlPressed) {
+//     const selectedBlock = getSelectedBlock(flowSlice);
+//     if (selectedBlock) {
+//       flowSlice.createBlockCopy(Math.round(pos.x), Math.round(pos.y));
+//     }
+//   }
+// }
