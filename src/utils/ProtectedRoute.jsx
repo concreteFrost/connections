6a5bd375bd9df85e51ebc793d549d1f094c2dbe6
concreteFrost <ilia.m.composer@gmail.com>
2024-08-20 13:17:44 +0100

    changed logout procedure@

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -1,17 +1,14 @@
 import { getAccessToken } from "../store/actions/storageActions";
 import { useEffect } from "react";
 import { clearUserData } from "../store/actions/storageActions";
-import useStore from "store/store";
 
 const ProtectedRoute = ({ children }) => {
   const user = getAccessToken();
-  const {deleteCurrentUser}= useStore((state)=>state.securitySlice)
 
   //Redirects if the session expired
   useEffect(() => {
     if (!user.token || new Date() > new Date(user.expires)) {
       clearUserData();
-      deleteCurrentUser();
       window.location.href = "/login"; // Navigate to the login page
     }
   }, []);
